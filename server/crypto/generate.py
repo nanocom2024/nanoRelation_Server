60048c4d117cdaa6d8da1a89d4725900fa00a5d8
k22036 <131113333+k22036@users.noreply.github.com>
2024-08-18 21:38:27 +0900

    test: 鍵の生成のテストコードの追加

--- server/crypto/generate.py
+++ server/crypto/generate.py
@@ -35,3 +35,14 @@ if __name__ == '__main__':
     private_key, public_key = generate_ed25519_keypair()
     print(f'private_key: {private_key}')
     print(f'public_key: {public_key}')
+
+    # 本当に公開鍵から秘密鍵を復元できるか確認
+    private_key_bytes = bytes.fromhex(private_key)
+    public_key_bytes = bytes.fromhex(public_key)
+    private_key = Ed25519PrivateKey.from_private_bytes(private_key_bytes)
+    public_key = private_key.public_key()
+    assert public_key_bytes == public_key.public_bytes(
+        encoding=serialization.Encoding.Raw,
+        format=serialization.PublicFormat.Raw
+    )
+    print('Test passed')
