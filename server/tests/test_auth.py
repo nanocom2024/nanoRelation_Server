969661f21e143d0f1d2242df8783a78949e6c19c
Hyouhyan <hyouhyan@hyouhyan.com>
2024-08-19 17:11:15 +0900

    feat: pytest実行時にbaseurlパラメータを受け取るようにした

--- server/tests/test_auth.py
+++ server/tests/test_auth.py
@@ -1,11 +1,21 @@
 import requests
+import pytest
 
 token = ''
 
+def pytest_addoption(parser):
+    parser.addoption('--baseurl', # オプション名
+                    default='http://127.0.0.1:8181' # デフォルト値
+                    )
 
-def test_signup_success():
+@pytest.fixture
+def baseurl(request):
+    return request.config.getoption("--baseurl")
+
+
+def test_signup_success(baseurl):
     global token
-    url = 'http://127.0.0.1:5000/signup'
+    url = baseurl+'/signup'
     res = requests.post(url, json={
         'name': 'test',
         'email': 'test@test.org',
@@ -16,8 +26,8 @@ def test_signup_success():
     assert token
 
 
-def test_signup_missing_name():
-    url = 'http://127.0.0.1:5000/signup'
+def test_signup_missing_name(baseurl):
+    url = baseurl+'/signup'
     res = requests.post(url, json={
         'name': '',
         'email': 'test@test.org',
@@ -27,8 +37,8 @@ def test_signup_missing_name():
     assert res.json()['error'] == 'Missing name'
 
 
-def test_signup_missing_email():
-    url = 'http://127.0.0.1:5000/signup'
+def test_signup_missing_email(baseurl):
+    url = baseurl+'/signup'
     res = requests.post(url, json={
         'name': 'test',
         'email': '',
@@ -38,8 +48,8 @@ def test_signup_missing_email():
     assert res.json()['error'] == 'Missing email'
 
 
-def test_signup_missing_password():
-    url = 'http://127.0.0.1:5000/signup'
+def test_signup_missing_password(baseurl):
+    url = baseurl+'/signup'
     res = requests.post(url, json={
         'name': 'test',
         'email': 'test@test.org',
@@ -49,8 +59,8 @@ def test_signup_missing_password():
     assert res.json()['error'] == 'Missing password'
 
 
-def test_signup_already_exist():
-    url = 'http://127.0.0.1:5000/signup'
+def test_signup_already_exist(baseurl):
+    url = baseurl+'/signup'
     res = requests.post(url, json={
         'name': 'test',
         'email': 'test@test.org',
@@ -61,8 +71,8 @@ def test_signup_already_exist():
     assert res.json()['error'] == expected
 
 
-def test_signout_missing_token():
-    url = 'http://127.0.0.1:5000/signout'
+def test_signout_missing_token(baseurl):
+    url = baseurl+'/signout'
     res = requests.post(url, json={
         'token': ''
     })
@@ -70,8 +80,8 @@ def test_signout_missing_token():
     assert res.json()['error'] == 'Invalid token'
 
 
-def test_signout_invalid_token():
-    url = 'http://127.0.0.1:5000/signout'
+def test_signout_invalid_token(baseurl):
+    url = baseurl+'/signout'
     res = requests.post(url, json={
         'token': 'invalidToken'
     })
@@ -79,9 +89,9 @@ def test_signout_invalid_token():
     assert res.json()['error'] == 'Invalid token'
 
 
-def test_signout_success():
+def test_signout_success(baseurl):
     global token
-    url = 'http://127.0.0.1:5000/signout'
+    url = baseurl+'/signout'
     res = requests.post(url, json={
         'token': token
     })
@@ -89,9 +99,9 @@ def test_signout_success():
     assert res.json() == {'done': 'success'}
 
 
-def test_signin_success():
+def test_signin_success(baseurl):
     global token
-    url = 'http://127.0.0.1:5000/signin'
+    url = baseurl+'/signin'
     res = requests.post(url, json={
         'email': 'test@test.org',
         'password': 'password'
@@ -101,8 +111,8 @@ def test_signin_success():
     assert token
 
 
-def test_signin_missing_email():
-    url = 'http://127.0.0.1:5000/signin'
+def test_signin_missing_email(baseurl):
+    url = baseurl+'/signin'
     res = requests.post(url, json={
         'email': '',
         'password': 'password'
@@ -111,8 +121,8 @@ def test_signin_missing_email():
     assert res.json()['error'] == 'Missing email'
 
 
-def test_signin_missing_password():
-    url = 'http://127.0.0.1:5000/signin'
+def test_signin_missing_password(baseurl):
+    url = baseurl+'/signin'
     res = requests.post(url, json={
         'email': 'test@test.org',
         'password': ''
@@ -121,8 +131,8 @@ def test_signin_missing_password():
     assert res.json()['error'] == 'Missing password'
 
 
-def test_signin_wrong_email():
-    url = 'http://127.0.0.1:5000/signin'
+def test_signin_wrong_email(baseurl):
+    url = baseurl+'/signin'
     res = requests.post(url, json={
         'email': 'wrongEmail@test.org',
         'password': 'password'
@@ -132,8 +142,8 @@ def test_signin_wrong_email():
     assert res.json()['error'] == expected
 
 
-def test_signin_wrong_password():
-    url = 'http://127.0.0.1:5000/signin'
+def test_signin_wrong_password(baseurl):
+    url = baseurl+'/signin'
     res = requests.post(url, json={
         'email': 'test@test.org',
         'password': 'wrongPassword'
@@ -143,9 +153,9 @@ def test_signin_wrong_password():
     assert res.json()['error'] == expected
 
 
-def test_delete_account_missing_password():
+def test_delete_account_missing_password(baseurl):
     global token
-    url = 'http://127.0.0.1:5000/delete_account'
+    url = baseurl+'/delete_account'
     res = requests.post(url, json={
         'password': '',
         'confirmPassword': 'password',
@@ -155,9 +165,9 @@ def test_delete_account_missing_password():
     assert res.json()['error'] == 'Missing password'
 
 
-def test_delete_account_missing_confirm_password():
+def test_delete_account_missing_confirm_password(baseurl):
     global token
-    url = 'http://127.0.0.1:5000/delete_account'
+    url = baseurl+'/delete_account'
     res = requests.post(url, json={
         'password': 'password',
         'confirmPassword': '',
@@ -167,8 +177,8 @@ def test_delete_account_missing_confirm_password():
     assert res.json()['error'] == 'Missing confirmPassword'
 
 
-def test_delete_account_missing_token():
-    url = 'http://127.0.0.1:5000/delete_account'
+def test_delete_account_missing_token(baseurl):
+    url = baseurl+'/delete_account'
     res = requests.post(url, json={
         'password': 'password',
         'confirmPassword': 'password',
@@ -178,9 +188,9 @@ def test_delete_account_missing_token():
     assert res.json()['error'] == 'Invalid token'
 
 
-def test_delete_account_passwords_do_not_match():
+def test_delete_account_passwords_do_not_match(baseurl):
     global token
-    url = 'http://127.0.0.1:5000/delete_account'
+    url = baseurl+'/delete_account'
     res = requests.post(url, json={
         'password': 'password',
         'confirmPassword': 'wrongPassword',
@@ -190,8 +200,8 @@ def test_delete_account_passwords_do_not_match():
     assert res.json()['error'] == 'Passwords do not match'
 
 
-def test_delete_account_invalid_token():
-    url = 'http://127.0.0.1:5000/delete_account'
+def test_delete_account_invalid_token(baseurl):
+    url = baseurl+'/delete_account'
     res = requests.post(url, json={
         'password': 'password',
         'confirmPassword': 'password',
@@ -201,9 +211,9 @@ def test_delete_account_invalid_token():
     assert res.json()['error'] == 'Invalid token'
 
 
-def test_delete_account_invalid_password():
+def test_delete_account_invalid_password(baseurl):
     global token
-    url = 'http://127.0.0.1:5000/delete_account'
+    url = baseurl+'/delete_account'
     res = requests.post(url, json={
         'password': 'invalidPassword',
         'confirmPassword': 'invalidPassword',
@@ -213,9 +223,9 @@ def test_delete_account_invalid_password():
     assert res.json()['error'] == 'INVALID_LOGIN_CREDENTIALS'
 
 
-def test_delete_account_success():
+def test_delete_account_success(baseurl):
     global token
-    url = 'http://127.0.0.1:5000/delete_account'
+    url = baseurl+'/delete_account'
 
     res = requests.post(url, json={
         'password': 'password',
