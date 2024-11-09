f4c1ca0552793afa4eedd0bc213ea10891e693f2
hyouhyan <hyouhyan@hyouhyan.com>
2024-11-09 23:07:40 +0900

    feat: フレンド追加・削除・取得をAPIでできるように

--- server/src/Friend/routes.py
+++ server/src/Friend/routes.py
@@ -12,7 +12,7 @@ db = DB()
 def add_friend():
     req_token = request.json['token']
     if not req_token: return jsonify({'error': 'Missing token'}), 400
-    
+
     req_users = request.json['Users']
     if not req_users: return jsonify({'error': 'User not defined'}), 400
 
@@ -20,10 +20,20 @@ def add_friend():
         if not user['name']: return jsonify({'error': 'UserName not defined'}), 400
         if not user['id']: return jsonify({'error': 'UserID not defined'}), 400
 
+        uid = db.users.find_one({'token': req_token})['uid']
+        manageFriend.add_friend(uid, user['uid'])
+
+    return jsonify({'message': 'Success'}), 200
+
 @FRIEND_BP.route('/get', methods=['POST'])
 def get_friend():
     req_token = request.json['token']
     if not req_token: return jsonify({'error': 'Missing token'}), 400
+
+    uid = db.users.find_one({'token': req_token})['uid']
+    friends = manageFriend.get_friends(uid)
+
+    return jsonify({'friends': friends}), 200
 
 @FRIEND_BP.route('/delete', methods=['POST'])
 def delete_friend():
@@ -36,3 +46,8 @@ def delete_friend():
     for user in req_users:
         if not user['name']: return jsonify({'error': 'UserName not defined'}), 400
         if not user['id']: return jsonify({'error': 'UserID not defined'}), 400
+
+        uid = db.users.find_one({'token': req_token})['uid']
+        manageFriend.remove_friend(uid, user['uid'])
+
+    return jsonify({'message': 'Success'}), 200
