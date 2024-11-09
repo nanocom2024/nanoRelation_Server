130aebc8f96844edf710629217f6575683872c9c
hyouhyan <hyouhyan@hyouhyan.com>
2024-11-09 22:03:17 +0900

    refactor: リクエストによる変数の名称変更

--- server/src/Friend/routes.py
+++ server/src/Friend/routes.py
@@ -8,29 +8,29 @@ db = DB()
 
 @FRIEND_BP.route('/add', methods=['POST'])
 def add_friend():
-    token = request.json['token']
-    if not token: return jsonify({'error': 'Missing token'}), 400
+    req_token = request.json['token']
+    if not req_token: return jsonify({'error': 'Missing token'}), 400
     
-    users = request.json['Users']
-    if not users: return jsonify({'error': 'User not defined'}), 400
+    req_users = request.json['Users']
+    if not req_users: return jsonify({'error': 'User not defined'}), 400
 
-    for user in users:
+    for user in req_users:
         if not user['name']: return jsonify({'error': 'UserName not defined'}), 400
         if not user['id']: return jsonify({'error': 'UserID not defined'}), 400
 
 @FRIEND_BP.route('/get', methods=['POST'])
 def add_friend():
-    token = request.json['token']
-    if not token: return jsonify({'error': 'Missing token'}), 400
+    req_token = request.json['token']
+    if not req_token: return jsonify({'error': 'Missing token'}), 400
 
 @FRIEND_BP.route('/delete', methods=['POST'])
 def add_friend():
-    token = request.json['token']
-    if not token: return jsonify({'error': 'Missing token'}), 400
+    req_token = request.json['token']
+    if not req_token: return jsonify({'error': 'Missing token'}), 400
 
-    users = request.json['Users']
-    if not users: return jsonify({'error': 'User not defined'}), 400
+    req_users = request.json['Users']
+    if not req_users: return jsonify({'error': 'User not defined'}), 400
 
-    for user in users:
+    for user in req_users:
         if not user['name']: return jsonify({'error': 'UserName not defined'}), 400
         if not user['id']: return jsonify({'error': 'UserID not defined'}), 400
