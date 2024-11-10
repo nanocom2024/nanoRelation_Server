cac64bb671ba2d1ef2717e1a892ee04243caeda0
hyouhyan <hyouhyan@hyouhyan.com>
2024-11-10 15:33:58 +0900

    fix: KeyError回避
    ifの検証方法をKeyの有無に変更

--- server/src/Friend/routes.py
+++ server/src/Friend/routes.py
@@ -10,15 +10,17 @@ db = DB()
 
 @FRIEND_BP.route('/add', methods=['POST'])
 def add_friend():
+    if not 'token' in request.json : return jsonify({'error': 'Missing token'}), 400
     req_token = request.json['token']
-    if not req_token: return jsonify({'error': 'Missing token'}), 400
 
-    req_users = request.json['Users']
+    if not 'users' in request.json: return jsonify({'error': 'User not defined'}), 400
+    req_users = request.json['users']
+
     if not req_users: return jsonify({'error': 'User not defined'}), 400
 
     for user in req_users:
-        if not user['name']: return jsonify({'error': 'UserName not defined'}), 400
-        if not user['id']: return jsonify({'error': 'UserID not defined'}), 400
+        if not 'name' in user: return jsonify({'error': 'UserName not defined'}), 400
+        if not 'id' in user: return jsonify({'error': 'UserID not defined'}), 400
 
         uid = db.users.find_one({'token': req_token})['uid']
         manageFriend.add_friend(uid, user['uid'])
@@ -27,8 +29,8 @@ def add_friend():
 
 @FRIEND_BP.route('/get', methods=['POST'])
 def get_friend():
+    if not 'token' in request.json: return jsonify({'error': 'Missing token'}), 400
     req_token = request.json['token']
-    if not req_token: return jsonify({'error': 'Missing token'}), 400
 
     uid = db.users.find_one({'token': req_token})['uid']
     friends = manageFriend.get_friends(uid)
@@ -38,15 +40,17 @@ def get_friend():
 # TODO: getだけどPOSTでいいのか？
 @FRIEND_BP.route('/delete', methods=['POST'])
 def delete_friend():
+    if not 'token' in request.json: return jsonify({'error': 'Missing token'}), 400
     req_token = request.json['token']
-    if not req_token: return jsonify({'error': 'Missing token'}), 400
 
-    req_users = request.json['Users']
+    if not 'users' in request.json: return jsonify({'error': 'User not defined'}), 400
+    req_users = request.json['users']
+
     if not req_users: return jsonify({'error': 'User not defined'}), 400
 
     for user in req_users:
-        if not user['name']: return jsonify({'error': 'UserName not defined'}), 400
-        if not user['id']: return jsonify({'error': 'UserID not defined'}), 400
+        if 'name' not in user: return jsonify({'error': 'UserName not defined'}), 400
+        if 'id' not in user: return jsonify({'error': 'UserID not defined'}), 400
 
         uid = db.users.find_one({'token': req_token})['uid']
         manageFriend.remove_friend(uid, user['uid'])
