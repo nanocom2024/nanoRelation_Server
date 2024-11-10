79d2816e46d4b84fb01cf636553bd3d0d7a56daf
hyouhyan <hyouhyan@hyouhyan.com>
2024-11-10 15:38:14 +0900

    feat: フレンドをnameとidから特定、ユーザーのトークン検証とNoneTypeエラー回避

--- server/src/Friend/routes.py
+++ server/src/Friend/routes.py
@@ -22,8 +22,15 @@ def add_friend():
         if not 'name' in user: return jsonify({'error': 'UserName not defined'}), 400
         if not 'id' in user: return jsonify({'error': 'UserID not defined'}), 400
 
-        uid = db.users.find_one({'token': req_token})['uid']
-        manageFriend.add_friend(uid, user['uid'])
+        uid = db.users.find_one({'token': req_token})
+        if not uid: return jsonify({'error': 'Invalid token'}), 400
+        uid = uid['uid']
+
+        friend_uid = db.users.find_one({'name': user['name'], 'uid': user['id']})
+        if not friend_uid: return jsonify({'error': 'User not found'}), 400
+        friend_uid = friend_uid['uid']
+
+        manageFriend.add_friend(uid, friend_uid)
 
     return jsonify({'message': 'Success'}), 200
 
@@ -32,8 +39,9 @@ def get_friend():
     if not 'token' in request.json: return jsonify({'error': 'Missing token'}), 400
     req_token = request.json['token']
 
-    uid = db.users.find_one({'token': req_token})['uid']
+    uid = db.users.find_one({'token': req_token})
     friends = manageFriend.get_friends(uid)
+    uid = uid['uid']
 
     return jsonify({'friends': friends}), 200
 
@@ -52,7 +60,14 @@ def delete_friend():
         if 'name' not in user: return jsonify({'error': 'UserName not defined'}), 400
         if 'id' not in user: return jsonify({'error': 'UserID not defined'}), 400
 
-        uid = db.users.find_one({'token': req_token})['uid']
-        manageFriend.remove_friend(uid, user['uid'])
+        uid = db.users.find_one({'token': req_token})
+        if not uid: return jsonify({'error': 'Invalid token'}), 400
+        uid = uid['uid']
+
+        friend_uid = db.users.find_one({'name': user['name'], 'uid': user['id']})
+        if not friend_uid: return jsonify({'error': 'User not found'}), 400
+        friend_uid = friend_uid['uid']
+
+        manageFriend.remove_friend(uid, friend_uid)
 
     return jsonify({'message': 'Success'}), 200
