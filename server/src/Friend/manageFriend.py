70235464571b5ca7f4fff1a36b3e7f3f9ecb9198
k22036 <131113333+k22036@users.noreply.github.com>
2024-11-22 09:07:21 +0900

    change: 友達リストの取得するデータの変更

--- server/src/Friend/manageFriend.py
+++ server/src/Friend/manageFriend.py
@@ -4,13 +4,13 @@ from DB import DB
 
 db = DB()
 
-users = db.users
 friends = db.friends
+
 
 def integrity():
     # usersDBから全ユーザーのuidを取得
     uids = []
-    for user in users.find():
+    for user in db.users.find():
         uids.append(user["uid"])
 
     # friendsDBにuidを追加
@@ -30,7 +30,8 @@ def integrity():
 def add_friend(uid, friend_uid):
     # uidが存在しない場合、エラー
     if not friends.find_one({"uid": uid}):
-        if users.find_one({"uid": uid}): integrity()
+        if db.users.find_one({"uid": uid}):
+            integrity()
         else:
             print("uid not found")
             return False
@@ -41,16 +42,18 @@ def add_friend(uid, friend_uid):
 
     # frined_uidの有効性を確認
     if not friends.find_one({"uid": friend_uid}):
-        if users.find_one({"uid": friend_uid}): integrity()
+        if db.users.find_one({"uid": friend_uid}):
+            integrity()
         else:
             print("friend_uid not found")
             return False
 
     # フレンドのuidを追加
-    friends.update_one( {"uid": uid}, {"$push": {"friends": friend_uid}} )
+    friends.update_one({"uid": uid}, {"$push": {"friends": friend_uid}})
 
     # 相手の方にも自分のuidを追加
-    friends.update_one( {"uid": friend_uid}, {"$push": {"friends": uid}} )
+    friends.update_one({"uid": friend_uid}, {"$push": {"friends": uid}})
+
 
 def remove_friend(uid, friend_uid):
     # uidが存在しない場合、エラー
@@ -68,10 +71,11 @@ def remove_friend(uid, friend_uid):
         return False
 
     # フレンドのuidを削除
-    friends.update_one( {"uid": uid}, {"$pull": {"friends": friend_uid}} )
+    friends.update_one({"uid": uid}, {"$pull": {"friends": friend_uid}})
 
     # 相手側から自分を削除
-    friends.update_one( {"uid": friend_uid}, {"$pull": {"friends": uid}} )
+    friends.update_one({"uid": friend_uid}, {"$pull": {"friends": uid}})
+
 
 def get_friends(uid):
     # uidが存在しない場合、エラー
@@ -79,5 +83,24 @@ def get_friends(uid):
         print("uid not found")
         return False
 
+    users = fetch_users()
+    res = []
+    for friend_uid in friends.find_one({"uid": uid})["friends"]:
+        if friend_uid in users:
+            res.append(users[friend_uid])
+
     # フレンドのuidを取得
-    return friends.find_one({"uid": uid})["friends"]
+    return res
+
+
+def fetch_users():
+    """
+    usersを取得する
+
+    :return users: list[dict]
+    """
+
+    data = db.users.find()
+    res = {row["uid"]: {"uid": row["uid"], "name": row["name"], "name_id": row["name_id"]}
+           for row in data}
+    return res
